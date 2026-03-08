9f2e7f1ac7d94bb59cc5bba8fb2040a25f4e2988
desarolladorsoftware-code <desarollador.software@cedull.edu.pe>
2026-03-08 00:28:06 -0500

    Estilo de cert v6 commit

--- app/api/certificates/[id]/pdf/route.ts
+++ app/api/certificates/[id]/pdf/route.ts
@@ -258,7 +258,7 @@ export async function GET(
       header: {
         logoRight: 52,
         logoTop: 70,
-        logoScale: 0.042,
+        logoScale: 0.999,
 
         institutionalTextY: height - 92,
         titleY: height - 148,
